7b8b2bb1a6237901cc6d7c275e1d19b888d0aa73
arakonon <arakonon@gmail.com>
2025-05-11 23:52:25 +0200

    checkQuality mit 3 stufen eingebaut und Ampel.py angepasst

--- innenstation/Ampel.py
+++ innenstation/Ampel.py
@@ -1,26 +1,34 @@
 from gpiozero import LED
 from time import sleep
+from checkQuality import checkQuality
 
 class Ampel:
     def __init__(self):
-                
+        
+        self.alarm = checkQuality()  # Instanz der Alarm-Klasse erstellen
         # Definiere die LEDs
         self.leds = [LED(26), LED(5), LED(6)]
         self.running = False  # Flag, um den Thread zu stoppen?
 
-    def start(self, get_humidity):
-        """Starte die Ampelsteuerung basierend auf der Luftfeuchtigkeit."""
+    def start(self):
+        """Starte die LED-Ampel (Rot, Gelb, Grün)."""
         self.running = True
         while self.running:
-            humidity = get_humidity()  # Luftfeuchtigkeit abrufen
-            if humidity < 35 or humidity > 65:
-                self._set_leds([1, 0, 0])  # Rot
-            elif 35 <= humidity < 40 or 60 < humidity <= 65:
-                self._set_leds([0, 1, 0])  # Gelb
-            else:
-                self._set_leds([0, 0, 0])  # Grün/aus
+            self.alarm.update_values()  # Sensorwerte aktualisieren
 
-            sleep(1)
+            iaq_status  = self.alarm.check_IAQ_quality()
+            co2_status  = self.alarm.check_CO2_quality()
+            hum_status  = self.alarm.check_humidity_quality()
+
+            # Gesamtergebnis (Rot dominiert vor Gelb vor Grün)
+            if   0 in (iaq_status, co2_status, hum_status):
+                self._set_leds([1, 0, 0])   # Rot
+            elif 1 in (iaq_status, co2_status, hum_status):
+                self._set_leds([0, 1, 0])   # Gelb
+            else:
+                self._set_leds([0, 0, 1])   # Grün
+
+            sleep(1)   # 1-s-Takt; bei Bedarf anpassen
 
     def stop(self):
         """Stoppe die Ampelsteuerung."""
--- innenstation/BurnTest.py
+++
@@ -1,6 +0,0 @@
-from BME680 import BME680
-
-bme = BME680()
-
-bme._data()
-print(bme.read_temperature())
