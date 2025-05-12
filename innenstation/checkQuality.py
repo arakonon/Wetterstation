993a85db06a6b7d007a097bae0dd223941f60f8c
arakonon <arakonon@gmail.com>
2025-05-12 19:12:21 +0200

    Display gemacht, Ampel akuatellisiert

--- innenstation/checkQuality.py
+++ innenstation/checkQuality.py
@@ -1,5 +1,5 @@
 from BME680 import BME680
-import time
+#import time
 
 class checkQuality:
 
@@ -8,19 +8,27 @@ class checkQuality:
 
     def update_values(self):
         """Aktualisiere die Sensorwerte."""
-        iaq, acc = self.bme.read_iaq()
-        co2, acc  = self.bme.read_co2()
-        hum = self.bme.read_humidity()
+        self.iaq, self.iaq_acc = self.bme.read_iaq()
+        self.co2, self.co2_acc  = self.bme.read_co2()
+        self.hum = self.bme.read_humidity()
 
-        print(f"[update_values] IAQ {self.iaq:.1f} (Acc {self.acc})")
-        print(f"[update_values] CO₂ {self.co2:.1f} (Acc {self.acc})")
-        print(f"[update_values] Hum {self.hum:.1f}")
+        #print(f"[update_values] IAQ {self.iaq:.1f} (Acc {self.iaq_acc})")
+        #print(f"[update_values] CO₂ {self.co2:.1f} (Acc {self.co2_acc})")
+        #print(f"[update_values] Hum {self.hum:.1f}")
+
+    def update_values_check_acc(self):
+        self.iaq, self.iaq_acc = self.bme.read_iaq()
+        self.co2, self.co2_acc  = self.bme.read_co2()
+        self.hum = self.bme.read_humidity()
+
+        print("[checkQuality-update_values_check_acc] loop bis acc >= 2")
+
 
 
 
     def check_IAQ_quality(self):
         """Überprüfe die Luftqualität und aktiviere den Alarm bei Bedarf."""
-        if self.acc >= 2:
+        if self.iaq_acc >= 2:
             #print(f"[check_IAQ_quality] IAQ {self.iaq:.1f} (Acc {self.acc})")
             if self.iaq > 150:  # Lüften
                 print("[check_air_quality] Rot")
@@ -37,7 +45,7 @@ class checkQuality:
 
     def check_CO2_quality(self):
         """Überprüfe die CO₂-Qualität und aktiviere den Alarm bei Bedarf."""
-        if self.acc >= 2:
+        if self.co2_acc >= 2:
             #print(f"[check_air_quality] CO₂ {self.co2:.1f} (Acc {self.acc})")
             if self.co2 > 1500:  # Lüften
                 print("[check_CO2_quality] Rot")
@@ -62,5 +70,15 @@ class checkQuality:
         else:
             print("[check_humidity_quality] Grün")
             return 2
+        
+    def  check_acc(self):
+        checkQuality.update_values_check_acc(self)
+        """Überprüfe die Kalibrierung."""
+        if self.iaq_acc < 2:
+            #print("[check_acc] Kalibrierung läuft …")
+            return True
+        else:
+            print("[check_acc] Kalibrierung abgeschlossen.")
+            return False
         
         
