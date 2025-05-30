eec343fac51e731c14552ffaf36def7868861627
Arakonon <Arakonon@gmail.com>
2025-05-30 19:00:29 +0200

    uv daten eingepflegt

--- innenstation/Datenbank.py
+++ innenstation/Datenbank.py
@@ -21,16 +21,17 @@ class Datenbank:
         # Letzter ESP-Wert
         self._ext_temp = None
         self._ext_hum = None
+        self._ext_uv = None      
         self._last_write = time.time()
 
         self._header = [
             "timestamp", "iaq", "co2_ppm",
             "hum_rel", "temp_c",
-            "temp_out", "hum_out"
+            "temp_out", "hum_out", "uv_out"
         ]
 
     # ---------- öffentliche API ----------
-    def log_row(self, sensor, temp_out=None, hum_out=None):
+    def log_row(self, sensor, temp_out=None, hum_out=None, uv_out=None):  
         """Alle 30 s aufrufen. Schreibt erst nach 300 s eine Zeile."""
 
         # Innenmessung in den Puffer
@@ -55,6 +56,8 @@ class Datenbank:
             self._ext_temp = temp_out
         if hum_out is not None:
             self._ext_hum  = hum_out
+        if uv_out is not None:                # <--- ergänzt
+            self._ext_uv = uv_out             # <--- ergänzt
 
         # Ist das Intervall abgelaufen?
         now = time.time()
@@ -74,6 +77,7 @@ class Datenbank:
             avg(self._sum_temp),
             "" if self._ext_temp is None else round(self._ext_temp, 2),
             "" if self._ext_hum  is None else round(self._ext_hum, 2),
+            "" if self._ext_uv   is None else round(self._ext_uv, 2),   
         ]
 
         # ----- CSV schreiben -----
